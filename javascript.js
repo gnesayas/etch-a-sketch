5eb67faa1dc82b33dbc65d241eeb98bd80929da2
Gabriel <63373336+gnesayas@users.noreply.github.com>
2023-08-24 19:30:57 -0500

    Randomize rgb value and implement a darkening effect, on mousedown only

--- javascript.js
+++ javascript.js
@@ -7,8 +7,22 @@ resetButton.addEventListener('click', () => {
         dimensions = parseInt(prompt(
             'Invalid input. Please enter a number between 1 and 100: '));
     }
+    setVisitValues(dimensions);
     createGrid(dimensions);
 });
+
+let visitValues = [];
+
+function setVisitValues(dimensions) {
+    visitValues = [];
+    for (let i = 0; i < dimensions; i++) {
+        const visitRow = [];
+        for (let j = 0; j < dimensions; j++) {
+            visitRow.push(0);
+        }
+        visitValues.push(visitRow);
+    }
+}
 
 const container = document.querySelector('.container');
 const CONTAINER_SIZE = 960;
@@ -30,11 +44,40 @@ function createGrid(dimensions) {
             square.style.width = SQUARE_DIM + 'px';
             square.style.height = SQUARE_DIM + 'px';
             square.style.outline = '1px solid black';
-            square.addEventListener('mouseover', () => {
-                square.style.background = 'red';
-            });
+            square.setAttribute('data-row', i);
+            square.setAttribute('data-col', j);
+            square.addEventListener('mouseover', changeBackgroundColor);
             row.appendChild(square);
         }
         container.appendChild(row);
+    }
+}
+
+let primaryMouseButtonDown = false;
+
+function setPrimaryButtonState(e) {
+    e.preventDefault();
+    let flags = e.buttons !== undefined ? e.buttons : e.which;
+    primaryMouseButtonDown = (flags & 1) === 1;
+}
+
+document.addEventListener('mousedown', setPrimaryButtonState);
+document.addEventListener('mousemove', setPrimaryButtonState);
+document.addEventListener('mouseup', setPrimaryButtonState);
+
+function changeBackgroundColor(e) {
+    e.preventDefault();
+    if (primaryMouseButtonDown) {
+        const row = parseInt(this.getAttribute('data-row'));
+        const col = parseInt(this.getAttribute('data-col'));
+        const timesVisited = Math.min(10, visitValues[row][col] + 1);
+        const r = Math.floor(Math.random() * 255);
+        const g = Math.floor(Math.random() * 255);
+        const b = Math.floor(Math.random() * 255);
+        const adjustedR = r * (10 - timesVisited) / 9;
+        const adjustedG = g * (10 - timesVisited) / 9;
+        const adjustedB = b * (10 - timesVisited) / 9;
+        this.style.background = `rgb(${adjustedR}, ${adjustedG}, ${adjustedB})`;
+        visitValues[row][col] += 1;
     }
 }
